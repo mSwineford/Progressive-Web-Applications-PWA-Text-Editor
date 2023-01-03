a36f63e2e7517b86c0d1d33e5a634f37e5aa309c
Matthew Swineford <m.swineford@yahoo.com>
2023-01-02 19:21:35 -0500

    finished all TODOs and got package. Now I need to fix errors involving npm run start:dev not working, although it was earlier.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new  HtmlWebpackPlugin({
+        title: "Webpack Plugin",
+        template: "./index.html"
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js"
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Another Text Editor",
+        theme_color: "#31a9e1",
+        background_color: "#31a9e1",
+        start_url: "/",
+        publicPath: "/",
+        inject: true,
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime"
+              ]
+            }
+          }
+        }
       ],
     },
   };
